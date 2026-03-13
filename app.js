a4eaab7ee6c8b2bbf39a71426b3d26c1204dd516
heckerman <sore.359400@malolos.sti.edu.ph>
2026-03-13 23:54:06 +0800

    add about me section

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const themeBtn = document.querySelector(".theme");
 const dlCv = document.getElementById("cv-btn");
+const githubBtn = document.querySelector(".github-btn");
 const profilePic = document.querySelector(".pfp");
+const aboutMe = document.querySelector(".about-me");
 themeBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
   if (document.body.classList.contains("dark-mode")) {
@@ -8,10 +10,12 @@ themeBtn.addEventListener("click", () => {
     themeBtn.style.borderColor = "#414144ff";
     themeBtn.style.backgroundColor = "#1f1f23ff";
     profilePic.src = "./assets/images/night-mode-pfp.png";
+    aboutMe.classList.add("dark-mode");
   } else {
     themeBtn.innerHTML = "<img src='./assets/images/moon.png' alt='' />";
-    themeBtn.style.borderColor = "#f1f2f3ff";
+    themeBtn.style.borderColor = "#949595ff";
     themeBtn.style.backgroundColor = "#f1f2f3ff";
     profilePic.src = "./assets/images/light-mode-pfp.png";
+    aboutMe.classList.remove("dark-mode");
   }
 });
