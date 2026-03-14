dbad5a16dda31d54c562c0988ed5a8af5abc5d3b
heckerman <sore.359400@malolos.sti.edu.ph>
2026-03-15 02:09:54 +0800

    add education section and mobile layout

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const themeBtn = document.querySelector(".theme");
 const dlCv = document.getElementById("cv-btn");
 const githubBtn = document.querySelector(".github-btn");
 const profilePic = document.querySelector(".pfp");
-const aboutMe = document.querySelector(".about-me");
+const cards = document.querySelectorAll(".cards");
+const infoIcon = document.querySelector(".info-icon");
+const gradcapIcon = document.querySelector(".gradcap-icon");
+console.log(cards);
 themeBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
   if (document.body.classList.contains("dark-mode")) {
@@ -10,12 +13,20 @@ themeBtn.addEventListener("click", () => {
     themeBtn.style.borderColor = "#414144ff";
     themeBtn.style.backgroundColor = "#1f1f23ff";
     profilePic.src = "./assets/images/night-mode-pfp.png";
-    aboutMe.classList.add("dark-mode");
+    infoIcon.src = "./assets/images/info-light.png";
+    gradcapIcon.src = "./assets/images/graduation-cap-light.png";
+    cards.forEach((card) => {
+      card.classList.add("dark-mode");
+    });
   } else {
     themeBtn.innerHTML = "<img src='./assets/images/moon.png' alt='' />";
     themeBtn.style.borderColor = "#949595ff";
     themeBtn.style.backgroundColor = "#f1f2f3ff";
     profilePic.src = "./assets/images/light-mode-pfp.png";
-    aboutMe.classList.remove("dark-mode");
+    infoIcon.src = "./assets/images/info-dark.png";
+    gradcapIcon.src = "./assets/images/graduation-cap-dark.png";
+    cards.forEach((card) => {
+      card.classList.remove("dark-mode");
+    });
   }
 });
